4eaac4b5b2802921c76a5fabadaeffa9fc9694e5
y0on2q <ltnscp9028@gmail.com>
2021-03-10 13:47:22 +0900

    build명령어 경로변경

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,22 +6,8 @@ import { createContext } from './context';
 const apollo = new ApolloServer({
   schema: schema,
   context: createContext(),
-  // schema: makeSchema({
-  //   types: type,
-  //   plugins: [nexusSchemaPrisma({ experimentalCRUD: true })],
-  //   sourceTypes: {
-  //     modules: [{ module: '.prisma/client', alias: 'PrismaClient' }],
-  //   },
-  //   contextType: {
-  //     module: path.join(__dirname, 'context.ts'),
-  //     export: 'Context',
-  //   },
-  //   outputs: {
-  //     typegen: path.join(__dirname, '/generated/nexus.ts'),
-  //     schema: path.join(__dirname, '/generated/schema.graphql'),
-  //   },
-  //   shouldExitAfterGenerateArtifacts: Boolean(process.env.NEXUS_SHOULD_EXIT_AFTER_REFLECTION),
-  // }),
+  introspection: true,
+  playground: true,
 });
 
 const app = express();
@@ -31,45 +17,3 @@ apollo.applyMiddleware({ app });
 app.listen(process.env.PORT, () => {
   console.log('GraphQL Server Start');
 });
-
-// const server = new GraphQLServer({
-//   schema: makeSchema({
-//     types: type,
-//     shouldGenerateArtifacts:process.env.NODE_ENV===
-//     plugins: [
-//       nexusSchemaPrisma({ experimentalCRUD: true }),
-//       // nexusSchemaPrisma({ experimentalCRUD: true }),
-//       // nexusSchemaPrisma({
-//       //   experimentalCRUD: true,
-//       // }),
-//     ],
-//     outputs: {
-//       schema: __dirname + '/generated/schema.graphql',
-//       typegen: __dirname + '/generated/nexus.ts',
-//     },
-//   }),
-//   context: (request) => {
-//     //if (!req.headers.Authorization) return { user: undefined };
-//     return {
-//       ...request,
-//       prisma,
-//     };
-//   },
-// });
-
-// // if (process.env.NODE_ENV == 'production') {
-// //   server.express.get('/', (req, res) => {
-// //     return res.status(200).send();
-// //   });
-// // }
-
-// const option = {
-//   // playground: process.env.NODE_ENV == 'production' ? false : '/',
-//   playground: '/',
-// };
-
-// server.start(option, async () => {
-//   await prisma.$connect();
-//   // checkTimer(admin);
-//   console.log('Listening on port 4000');
-// });
